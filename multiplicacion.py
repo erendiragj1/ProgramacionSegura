c32336c9841151348d6e32adde19946b7f7f657c
jbarradas980 <jbarradas.980718@gmail.com>
2020-04-15 18:53:26 -0500

    Se genera herramienta con métodos de multiplicación

---
+++ multiplicacion.py
@@ -0,0 +1,6 @@
+def multiplicar_numeros(num_a, num_b):
+    #JBarradas(15/04/2020): Función para multiplicar dos números
+    #    Parámetros:
+    #num_a : Primer número
+    #num_b : Segundo número
+    return num_a * num_b
