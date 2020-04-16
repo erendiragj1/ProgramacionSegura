394fc65945d66d72645912c7fd620c62701e0747
migu3l-hub <barney_eminemiguel@hotmail.com>
2020-04-15 22:18:03 -0500

    Se agrega la funcion de suma

---
+++ sumas.py
@@ -0,0 +1,7 @@
+def suma(n1,n2):
+    '''
+    :param n1: Es un numero entero
+    :param n2: Es un segundo numero entero
+    :return: Es un numero resultado de la suma de los dos numeros anteriores
+    '''
+    return n1+n2
