2ef223082b8939ba36a65c8b2bf53f92c14df2c8
erendiragj1 <42814751+erendiragj1@users.noreply.github.com>
2020-04-19 18:49:29 -0500

    Delete sumas.py

--- sumas.py
+++
@@ -1,7 +0,0 @@
-def suma(n1,n2):
-    '''
-    :param n1: Es un numero entero
-    :param n2: Es un segundo numero entero
-    :return: Es un numero resultado de la suma de los dos numeros anteriores
-    '''
-    return n1+n2 
