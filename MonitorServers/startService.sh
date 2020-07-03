f76f8f14edd8ea86de0440c0b3c656efd768923e
migu3l-hub <barney_eminemiguel@hotmail.com>
2020-07-03 12:07:20 -0500

    se cambia el arranque del servidor en startService.sh a 0.0.0.0:8000 para que se pueda conectar de todos lados

--- MonitorServers/startService.sh
+++ MonitorServers/startService.sh
@@ -8,5 +8,5 @@ done
 #python3 manage.py makemigrations
 #python3 manage.py migrate
 #python3 manage.py createsuperuser
-python3 manage.py runserver 127.0.0.1:8000
+python3 manage.py runserver 0.0.0.0:8000
 #python3 manage.py shell
