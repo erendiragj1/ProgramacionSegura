4bc0fac78a87d9b3d98f76a0684099f909e8d37c
erendiragj1 <42814751+erendiragj1@users.noreply.github.com>
2020-07-08 22:51:22 -0500

    actulizacion

--- MonitorServers/appMonitoreo/clases.py
+++
@@ -1,7 +0,0 @@
-
-class Monitor():
-    def __init__(self,cpu,memoria,disco):
-        self.cpu = cpu
-        self.memoria = memoria
-        self.disco = disco
-
--- MonitorServers/appMonitoreo/serializers.py
+++
@@ -1,8 +0,0 @@
-from .clases import Monitor
-from rest_framework import serializers
-
-
-class monitorSerializer(serializers.Serializer):
-    cpu = serializers.CharField(max_length=10)
-    memoria = serializers.CharField(max_length=10)
-    disco = serializers.CharField(max_length=10)
